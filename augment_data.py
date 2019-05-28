5a7183ac665ce6ad88ac376431edc140f37f7b6e
unknown <meontechno@gmail.com>
2019-05-28 12:13:23 -0700

    cmd line args edit

--- augment_data.py
+++ augment_data.py
@@ -4,14 +4,18 @@ from scipy import misc, ndimage
 import os
 import sys
 
-# Load images from the images dir
-IMG_DIR = sys.argv[1]
-AUG_DIR = sys.argv[1]
+if len(sys.argv) == 2 :
+    # Load images from the images dir
+    IMG_DIR = sys.argv[1]
+    AUG_DIR = sys.argv[1]
 
-for img in os.listdir(IMG_DIR):
-    aug_name = img.split(".", 1)
-    if(aug_name[1] == "jpg"):
-        ori_img = misc.imread(os.path.join(IMG_DIR, img))
-        ori_img = ndimage.gaussian_filter(ori_img, sigma=2)
-        dest_name = aug_name[0] + "gb." + aug_name[1]
-        misc.imsave( os.path.join(AUG_DIR, dest_name) , ori_img)
+    for img in os.listdir(IMG_DIR):
+        aug_name = img.split(".", 1)
+        if(aug_name[1] == "jpg"):
+            ori_img = misc.imread(os.path.join(IMG_DIR, img))
+            ori_img = ndimage.gaussian_filter(ori_img, sigma=2)
+            dest_name = aug_name[0] + "gb." + aug_name[1]
+            misc.imsave( os.path.join(AUG_DIR, dest_name) , ori_img)
+    print("\n Gaussian blur augmentation success!\n")
+else:
+    print("\n Please enter valid folder path: ./<folder> \n")
--- train_phone_finder.py
+++ train_phone_finder.py
@@ -14,96 +14,102 @@ from keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import TensorBoard 
 import keras.backend as K
 
-# Name for tensorboard logs
-NAME = "find_phone_train_CNN"
+if len(sys.argv) == 2:
+    # Name for tensorboard logs
+    NAME = "find_phone_train_CNN"
 
-# Directory where the images (including augmented) and labels exist 
-IMG_DIR = sys.argv[1]
+    # Directory where the images (including augmented) and labels exist 
+    IMG_DIR = sys.argv[1]
 
-# Parse labels.txt to find image, convert it into array and assign
-# values to the training_data variable
-data = pd.read_csv(os.path.join(IMG_DIR, "labels.txt"), sep=" ", header=None)
-data_rows = data.shape[0]
-data_cols = data.shape[1]
-training_data = []
-for x in range(data_rows):
-    ig_array = cv2.imread(os.path.join(IMG_DIR, data[0][x]))
-    training_data.append([ig_array, data[1][x], data[2][x]])
-
-
-# Split the training data into X input and Y output
-X = []
-Y = []
-for features, x_cord, y_cord in training_data:
-    X.append(features)
-    Y.append([x_cord, y_cord]) 
-X = np.array(X).reshape(-1, 326, 490, 3)
-Y = np.array(Y).reshape(-1, 2)
+    # Parse labels.txt to find image, convert it into array and assign
+    # values to the training_data variable
+    data = pd.read_csv(os.path.join(IMG_DIR, "labels.txt"), sep=" ", header=None)
+    data_rows = data.shape[0]
+    data_cols = data.shape[1]
+    training_data = []
+    for x in range(data_rows):
+        ig_array = cv2.imread(os.path.join(IMG_DIR, data[0][x]))
+        training_data.append([ig_array, data[1][x], data[2][x]])
 
 
-# *******************************************************
-# Building convolution network
-# *******************************************************
+    # Split the training data into X input and Y output
+    X = []
+    Y = []
+    for features, x_cord, y_cord in training_data:
+        X.append(features)
+        Y.append([x_cord, y_cord]) 
+    X = np.array(X).reshape(-1, 326, 490, 3)
+    Y = np.array(Y).reshape(-1, 2)
 
-inputs = Input(shape= X.shape[1:])
 
-# First convolution block
-l1 = Conv2D(64, (3,3), padding='same', strides=1)(inputs)
-l1 = ReLU()(l1)
+    # *******************************************************
+    # Building convolution network
+    # *******************************************************
 
-l2 = Conv2D(64, (3,3), padding='same', strides=1)(l1)
-l2 = ReLU()(l2)
+    inputs = Input(shape= X.shape[1:])
 
-l3 = Conv2D(64, (3,3), padding='same', strides=1)(l2)
-l3 = ReLU()(l3)
+    # First convolution block
+    l1 = Conv2D(64, (3,3), padding='same', strides=1)(inputs)
+    l1 = ReLU()(l1)
 
-p1 = MaxPooling2D(pool_size=(2,2))(l3)
-channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
-bn1 = BatchNormalization(axis=channel_axis)(p1)
-dp1 = Dropout(rate=0.2)(bn1)
+    l2 = Conv2D(64, (3,3), padding='same', strides=1)(l1)
+    l2 = ReLU()(l2)
 
-# Second convolution block
-l4 = Conv2D(128, (3,3), padding='same', strides=1)(dp1)
-l4 = ReLU()(l4)
+    l3 = Conv2D(64, (3,3), padding='same', strides=1)(l2)
+    l3 = ReLU()(l3)
 
-l5 = Conv2D(128, (3,3), padding='same', strides=1)(l4)
-l5 = ReLU()(l5)
+    p1 = MaxPooling2D(pool_size=(2,2))(l3)
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    bn1 = BatchNormalization(axis=channel_axis)(p1)
+    dp1 = Dropout(rate=0.2)(bn1)
 
-l6 = Conv2D(128, (3,3), padding='same', strides=1)(l5)
-l6 = ReLU()(l6)
+    # Second convolution block
+    l4 = Conv2D(128, (3,3), padding='same', strides=1)(dp1)
+    l4 = ReLU()(l4)
 
-p2 = MaxPooling2D(pool_size=(2,2))(l6)
-bn2 = BatchNormalization(axis=channel_axis)(p2)
-dp2 = Dropout(rate=0.2)(bn2)
+    l5 = Conv2D(128, (3,3), padding='same', strides=1)(l4)
+    l5 = ReLU()(l5)
 
-f1 = Flatten()(dp2)
+    l6 = Conv2D(128, (3,3), padding='same', strides=1)(l5)
+    l6 = ReLU()(l6)
 
-# Fully connected layers
-d1 = Dense(30)(f1)
-bn3 = BatchNormalization(axis=channel_axis)(d1)
-dp3 = Dropout(rate=0.2)(bn3)
+    p2 = MaxPooling2D(pool_size=(2,2))(l6)
+    bn2 = BatchNormalization(axis=channel_axis)(p2)
+    dp2 = Dropout(rate=0.2)(bn2)
 
-d2 = Dense(30)(dp3)
-bn4 = BatchNormalization(axis=channel_axis)(d2)
-dp4 = Dropout(rate=0.2)(bn4)
+    f1 = Flatten()(dp2)
 
-d3 = Dense(2)(dp4)
+    # Fully connected layers
+    d1 = Dense(30)(f1)
+    bn3 = BatchNormalization(axis=channel_axis)(d1)
+    dp3 = Dropout(rate=0.2)(bn3)
 
-model = Model(inputs=inputs, outputs=d3)
+    d2 = Dense(30)(dp3)
+    bn4 = BatchNormalization(axis=channel_axis)(d2)
+    dp4 = Dropout(rate=0.2)(bn4)
 
-# *******************************************************
-# End of neural network
-# *******************************************************
+    d3 = Dense(2)(dp4)
 
-model.compile(optimizer = "adam",
-              loss = "mse",
-              metrics = ["accuracy"])
+    model = Model(inputs=inputs, outputs=d3)
 
-# Creating tensorboard callback
-tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+    # *******************************************************
+    # End of neural network
+    # *******************************************************
 
-#model.summary()
-model.fit(X, Y, batch_size=5, epochs=200, validation_split=0.1, shuffle=True, callbacks=[tensorboard])
+    model.compile(optimizer = "adam",
+                loss = "mse",
+                metrics = ["accuracy"])
 
-# Save the model after training
-model.save("phone_detect.h5")
+    # Creating tensorboard callback
+    tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+    #model.summary()
+    model.fit(X, Y, batch_size=5, epochs=200, validation_split=0.1, shuffle=True, callbacks=[tensorboard])
+
+    # Save the model after training
+    model.save("phone_detect.h5")
+
+    print("\n Training complete! Model saved! \n")
+
+else:
+    print("\n Please enter valid folder path: ./<folder> \n")
