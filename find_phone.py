8e0d9cbbf07d804a5e2e01a0b5ae35d60ebd831e
unknown <meontechno@gmail.com>
2019-05-28 11:46:20 -0700

    find_phone inference

---
+++ find_phone.py
@@ -0,0 +1,24 @@
+
+# Importing data manipulation modules
+import numpy as np
+import os
+import sys
+import cv2
+
+# Importing NN modules
+from keras.models import Model, load_model
+
+# Reading image path argument from command line 
+img_path = sys.argv[1]
+
+# Loading keras model-
+model = load_model("saved_model/phone_detect.h5")
+
+# Read image into rgb numpy array
+test_img_array = cv2.imread(img_path)
+test_img_array = test_img_array.reshape(-1, 326, 490, 3)
+
+# Forward pass
+prediction = model.predict([test_img_array])
+
+print("\n\n" + str(prediction[0][0]) + " " + str(prediction[0][1]) + "\n\n")
